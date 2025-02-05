156211292844d1994a3c9f6875705221bddd1420
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-02-04 23:52:27 -0800

    03 visualize results script outline

---
+++ scripts/03_visualize_results.py
@@ -0,0 +1,7 @@
+# Import necessary libraries
+# Load simulation data from CSV
+# Generate plots:
+#     - Histogram of winnings
+#     - Box plot of winnings per strategy
+#     - Log-scale plot of winnings distribution
+# Save the plots in outputs/visualizations/
